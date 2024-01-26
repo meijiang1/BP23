43d03ad66b8c72b7583aa03c8274ae31070191b3
Mei Jiang <mei.jiang@student.kdg.be>
2024-01-26 05:58:34 +0100

    Added start and end scene
    Added script menu to switch scenes

--- ApartmentEscape/Assets/Sripts/GameManager.cs
+++ ApartmentEscape/Assets/Sripts/GameManager.cs
@@ -11,8 +11,6 @@ public class GameManager : MonoBehaviour
     
     //adjust with code
     public bool[] isRoomCorrect = new bool[5] {false, false, false, false, false};
-    //public bool[] haveBeenToCorrectRoom = new bool[5] { false, false, false, false, false };
-    //public bool[] haveBeenToRoom = new bool[5] { false, false, false, false, false };
     public int correctRoomCount = 0;
 
     public GameObject currentRoom = null;
@@ -34,56 +32,6 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.C)) { Debug.Log("Current room: " + currentRoom.name + currentRoom.tag); }
         if (Input.GetKeyDown(KeyCode.P)) { Debug.Log("Previous room: " + previousRoom.name + previousRoom.tag); }
-        if (Input.GetKeyDown(KeyCode.R)) { Debug.Log("isTeleported = " + isTeleported); }
-        if (Input.GetKeyDown(KeyCode.T)) { Debug.Log("isTeleporting = " + isTeleporting); }
         if (Input.GetKeyDown(KeyCode.Y)) { Debug.Log("teleportCount = " + teleportCount); }
-
-        
-
-        //if (teleportCount == 0)
-        //{
-        //    portals.SetActive(true);
-        //}
-
-        //if (!isTeleporting && teleportCount!= 0 && isTeleported)
-        //{
-        //    portals.SetActive(false);
-        //}
-        //else
-        //{
-        //    portals.SetActive(true);
-        //}
-
-        //if (isTeleported) { portals.SetActive(true); }else { portals.SetActive(false); }
-        //if (isTeleporting) { isTeleported = false; }
-
-
-        //if (isTeleporting) { portals.SetActive(false); } else { portals.SetActive(true); }
-
-        //if (previousroom != null)
-        //{
-        //    if ((currentroom != previousroom) && !isteleporting)
-        //    {
-        //        isteleported = true;
-        //    }
-        //    else
-        //    {
-        //        isteleported = true;
-        //    }
-        //}
     }
-
-    //void CheckRoom()
-    //{
-    //    foreach (GameObject room in playerPathRooms)
-    //    {
-    //        if (room != null) 
-    //        {
-    //            if (room == correctRoom[0])
-    //        }
-    //    }
-    //}
-
-    //player location
-    //end room = win
 }
--- ApartmentEscape/Assets/Sripts/WinLose.cs
+++ ApartmentEscape/Assets/Sripts/WinLose.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinLose : MonoBehaviour
 {
@@ -23,6 +24,7 @@ public class WinLose : MonoBehaviour
         {
             if (IsAllRoomCorrect())
             {
+                SceneManager.LoadScene(2);
                 Debug.Log("Player Win!");
             }
             else
